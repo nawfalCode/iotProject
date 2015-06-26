d9e2dd95169be0762ce4986d70e9a0128f557fc1
Nawfal Mahdi <nawfal.mahdi@hotmail.com>
2015-06-26 23:21:25 +1000

    initial commit

---
+++ models/device_model.js
@@ -0,0 +1,25 @@
+ var mongoose = require('mongoose');
+ var Schema = mongoose.Schema;
+ var deviceSchema = new Schema({
+     //Device ID: UUID format
+     deviceId: Number,
+     // Device description
+     deviceDescription: String,
+     //List of keywords (string)
+     deviceKeywords: Array,
+     // List of properties
+     deviceProperties: {
+         //location of the device (if possible)
+         location: {
+             longitude: String,
+             latitude: String,
+             accuracy: String
+         },
+         // Type of the device (if applicable)
+         deviceType: String,
+         //anything else
+         others: String
+     }
+ });
+ //create a model to be used later by the system
+ mongoose.model('device', deviceSchema);
---
+++ resources.js
@@ -0,0 +1,5 @@
+var resource1={
+    title: 'Create RESTful with Node, Mongo and Angular'
+    url:'http://cwbuecheler.com/web/tutorials/2014/restful-web-app-node-express-mongodb/'
+}
+
